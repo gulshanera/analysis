f99ad7c2f0c3d8a4da75a8d9fc6f4fd75660ae94
gulshanera <60612452+gulshanera@users.noreply.github.com>
2023-10-04 23:22:34 +0530

    Update and rename app.py to dewpoint.py

---
+++ dewpoint.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+# Load the data
+data_df = pd.read_excel("D:/air quality/OneDrive - design.iitd.ac.in/Desktop/Jodhpur_TB/Jodhpur_original/Raw_data/all_raw_new_hourly_shifted.xlsx")
+
+# Constants for dew point calculation
+a = 18.678
+b = 257.14
+
+# Ambient temperature and relative humidity
+T = data_df['Temperature']
+RH = data_df['Humidity'] / 100  # Assuming humidity is given in percentage
+
+# Alpha calculation
+alpha = (a * T) / (b + T) + np.log(RH)
+
+# Dew point calculation
+dew_point = (b * alpha) / (a - alpha)
+
+# Add the Dew_Point to the DataFrame for display
+data_df['Dew_Point'] = dew_point
+
+# Compare dew point with Filter_Temperature and create a new column with values "wet" or "dry"
+data_df['Condition'] = np.where(data_df['Enclosure_Temperature'] <= dew_point, 'wet', 'dry')
+
+# Display the first few rows including the new Condition column
+data_df[['Temperature', 'Humidity', 'Filter_Temperature', 'Dew_Point', 'Condition']].head()
+data_df.to_excel('Dewpoint.xlsx',index=False)
